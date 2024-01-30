e6c93f5ffb889f62cb906695a527f1004143846e
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-30 15:14:53 +0500

    web

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ bot = telegram.Bot(TOKEN)
 
 chat_id='927181585'
 
-@app.route('/' , methods=['POST'])
+@app.route('/' , methods=['GET'])
 def hello_world():
     bot.send_message(chat_id=chat_id , text='Hello World!!!')
     return 'Hello World!!'
