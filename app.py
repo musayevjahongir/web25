4df4e7ed37acbf261165f45732a06b0c1e7ec9ce
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-30 10:39:56 +0500

    python telegram bot

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import telegram
 
 app = Flask(__name__)
 
+TOKEN='6804364814:AAFG6WL6EcqnIgL_9qY4euWHUcvGaCKLU08'
+
+bot = telegram.Bot(TOKEN)
+
+chat_id='927181585'
+
 @app.route('/')
 def hello_world():
+    bot.send_message(chat_id=chat_id, text='Hello World!!!')
     return 'Hello World!!'
