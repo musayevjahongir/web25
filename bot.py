8fb09b862a1f1d5eb91bb3f74b0d495d47e9ad3b
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-30 12:26:39 +0500

    python telegram bot

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telegram 
+
+TOKEN='6804364814:AAFG6WL6EcqnIgL_9qY4euWHUcvGaCKLU08'
+
+bot = telegram.Bot(TOKEN)
+
+URL = 'https://jahongir5051.pythonanywhere.com/'
+
+bot.setWebhook(URL)
+
+print(bot.get_webhook_info())
